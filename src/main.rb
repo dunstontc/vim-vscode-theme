a05efe5754896653fec7438a076ebaff01642738
Clay Dunston <dunstontc@gmail.com>
2019-03-29 15:39:29 -0500

    refactored

--- src/main.rb
+++ src/main.rb
@@ -3,7 +3,6 @@ require 'json5'
 
 class Color
   attr_reader :name, :gui, :cterm, :cterm16
-
   def initialize(json_object)
     self.name    = json_object['name']
     self.gui     = json_object['gui']
@@ -12,17 +11,22 @@ class Color
   end
 end
 
-# TODO: guisp?
+def colors_from_hash_array(hash_array)
+  color_hash = {}
+  hash_array.each do |color|
+    color_hash.store(color['name'], color)
+  end
+  return color_hash
+end
+
 class HighlightGroup
-  attr_accessor :group, :fg, :bg, :styling
-
+  attr_accessor :group, :link, :fg, :bg, :styling
   def initialize(json_object)
-    @group = json_object['group']
-
-    @fg = json_object['style']['fg']
-    @bg = json_object['style']['bg']
-
-    @styling = json_object['style']['styling']
+    @group   = json_object['group']
+    @link    = json_object['link']             || nil
+    @fg      = json_object['style']['fg']      || nil
+    @bg      = json_object['style']['bg']      || nil
+    @styling = json_object['style']['styling'] || nil
   end
 end
 
@@ -51,7 +55,7 @@ class HighlightCommand
     if !hl_group.styling.nil?()
       @style_cmd = " cterm=#{hl_group.styling} gui=#{hl_group.styling}"
     else
-      @style_cmd = " cterm=NONE gui=NONE"
+      @style_cmd = ' cterm=NONE gui=NONE'
     end
   end
 
@@ -65,21 +69,33 @@ class HighlightCommand
   end
 end
 
-def colors_from_hash_array(hash_array)
-  color_hash = {}
-  hash_array.each do |color|
-    color_hash.store(color['name'], color)
-  end
-  return color_hash
+def link_command(hl_group)
+  return "highlight link #{hl_group['group']} #{hl_group['link']}"
 end
 
-groups_file = File.read('groups.json5')
-colors_file = File.read('colors.json')
+def header_text
+  template = '''
+  " ==============================================================================
+  " FILE: Dark Plus Vim
+  " Author: Clay Dunston <dunstontc@gmail.com>
+  " License: MIT License
+  " Last Modified: 2019-03-29
+  " ==============================================================================
+'''
+
+end
+
+groups_file = File.read('json/groups.json5')
+colors_file = File.read('json/colors.json')
 
 groups_data = JSON5.parse(groups_file)
 colors_data = colors_from_hash_array(JSON.parse(colors_file))
 
 groups_data.each do |data|
-  highlight = HighlightGroup.new(data)
-  puts HighlightCommand.new(highlight, colors_data).command
+  if data.key?('link')
+    puts link_command(data)
+  else
+    highlight = HighlightGroup.new(data)
+    puts HighlightCommand.new(highlight, colors_data).command
+  end
 end
